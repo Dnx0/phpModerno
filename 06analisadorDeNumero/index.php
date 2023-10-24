ce810d772a731661e4c20c433e7ed18fdfd4c25b
Victor Pereira <82619763+Dnx0@users.noreply.github.com>
2023-10-24 17:02:16 -0300

    SuperGlobais

--- 06analisadorDeNumero/index.php
+++ 06analisadorDeNumero/index.php
@@ -12,7 +12,7 @@
     <main>
         <section>
             <form action="numero.php" method="post">
-                <label for="numero">Digite o valor em reais:</label>
+                <label for="numero">Digite um numero racional com até 3 casas decimais:</label>
                 <!-- O step permite decimais no caso até 3, mas pode colocar mais 0 para adicionar mais casas decimais -->
                 <input type="number" name="numero" id='n' step="0.001" placeholder="Insira um numero racional">
                 <input type="submit" value="Analisar">
--- 06analisadorDeNumero/numero.php
+++ 06analisadorDeNumero/numero.php
@@ -4,8 +4,17 @@
     <h1>Analisador de Número Racional</h1>
     <?php 
     #Está pegando do formulario o valor que está no numero e colocando na variavel num, caso não tenha nada vai ser 0, com os ??
-        $num = $_POST["numero"] ?? 0;
+    //REQUEST pega tanto Post ou o Get
+        $num = $_REQUEST["numero"] ?? 0;
 
-        echo "<p> Analisando o número $num passado pelo usuário </p>";
+        # number fomart está colocando o número sempre com 3 casas decimais separando os milhares por virgula e o decimal por ponto
+        echo "<p> Analisando o número <strong>". number_format($num,3,",",".") ."</strong> passado pelo usuário </p>";
+
+        $int = (int) $num;
+        $frac = $num - $int;
+
+        echo "<ul><li>A parte inteira do número é: <strong>". number_format($int,0,",",".") ." </strong></li>";
+        echo "<li>A parte decimal do número é: <strong>". number_format($frac,3,",",".") ." </strong></li></ul>";
     ?>
+    <button onclick="javascript:history.go(-1)">← Voltar</button>
 </main>
